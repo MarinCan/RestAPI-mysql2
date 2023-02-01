c49e0906f04da0c4201856e4391b9a8c2891d2b6
Marta M <marmacan.upv@gmail.com>
2023-02-01 14:09:24 +0100

    Creada la BD

---
+++ db/databas.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db CHARSET utf8mb4;
+USE company_db;
+
+CREATE TABLE employee (
+    id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(50) DEFAULT NULL,
+    salary INT DEFAULT NULL
+);
+
+-- DESCRIBE employee;
+
+INSERT INTO employee() 
+VALUES ("");
