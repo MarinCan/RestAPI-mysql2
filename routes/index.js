c661c9e60babfa6961a4f3af7905d370932da643
Marta M <marmacan.upv@gmail.com>
2023-02-01 13:58:37 +0100

    Endpoints funcionales para el crud

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,25 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('Home');
 });
+
+/* GET home page. */
+router.get('/empleados', function(req, res, next) {
+  res.send('Obteniendo empleados');
+});
+
+router.post('/empleados', function(req, res, next) {
+  res.send('Creando empleados');
+});
+
+router.put('/empleados', function(req, res, next) {
+  res.send('Actualizando empleados');
+});
+
+router.delete('/empleados', function(req, res, next) {
+  res.send('Eliminando empleados');
+});
+
 
 module.exports = router;
