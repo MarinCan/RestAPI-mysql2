a0a31ade99fdc6d2253aabe4d224498b147fe26e
Marta M <marmacan.upv@gmail.com>
2023-02-03 10:18:59 +0100

    Añadidas funciones get y create empleados

--- controllers/empleados.controller.js
+++ controllers/empleados.controller.js
@@ -1,9 +1,23 @@
-const getEmpleados = (req, res, next) => {
-    res.send('Obteniendo empleados')
+const pool = require('../db')
+
+const getEmpleados = async (req, res, next) => {
+    const [rows] = await pool.query('SELECT * FROM employee')
+    res.json(rows)
 }
 
-const createEmpleados = (req, res, next) => {
-    res.send('Creando empleados')
+const createEmpleados = async (req, res, next) => {
+    const {name, salary} = req.body 
+    // console.log(req.body)
+    // console.log(name, salary)
+
+    const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary])
+
+    // res.send({rows})
+    res.send({
+        id: rows.insertId,
+        name,
+        salary
+    })
 }
 
 const updateEmpleados = (req, res, next) => {
